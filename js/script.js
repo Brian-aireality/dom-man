857fa2ccbc153702cad36760540a2e500a4c0117
Student <example@example.com>
2019-10-22 14:56:47 -0700

    add js.script change

--- js/script.js
+++ js/script.js
@@ -2,15 +2,31 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      document.getElementById("hello").className = "btn btn-danger";
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      document.getElementById("hello").className = "btn btn-danger";
+    });
   });
-
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      document.getElementById("goodbye").className = "btn btn-info";
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      document.getElementById("goodbye").className = "btn btn-info";
+    });
   });
-
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
